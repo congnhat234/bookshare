f738333e996fabbd6ad863f6a2722f93b473a910
congnhat234 <congnhat.234@gmail.com>
2019-11-12 14:33:18 +0700

    Fix for user registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,25 +42,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :address, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :address,
+      :avatar, :role])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :address, :avatar, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone,
+      :address, :avatar, :role])
   end
 
-  def after_update_path_for(resource)
+  def after_update_path_for resource
     user_path(resource)
   end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    root_path
+  def after_sign_up_path_for _resource
+    new_user_session_path
   end
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    root_path
+  def after_inactive_sign_up_path_for _resource
+    new_user_session_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :find_user, only: %i(show posts books)
+  before_action :authenticate_user!, only: %i(index)
   layout "dashboard/application", only: %i(index)
 
   def index
