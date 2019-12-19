919c84658ddbc429f8bbaf2fcadc93898b92f878
congnhat234 <congnhat.234@gmail.com>
2019-12-19 18:32:50 +0700

    Fix for redis db

--- config/initializers/classifier.rb
+++ config/initializers/classifier.rb
@@ -5,5 +5,5 @@ rails_root = Rails.root || File.dirname(__FILE__) + "/../.."
 rails_env = Rails.env || "development"
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 
-redis_backend = ClassifierReborn::BayesRedisBackend.new(:host => uri.host, :port => uri.port, :password => uri.password, :db => 1)
+redis_backend = ClassifierReborn::BayesRedisBackend.new(:host => uri.host, :port => uri.port, :password => uri.password, :db => 0)
 CLASSIFIER_POST = ClassifierReborn::Bayes.new "Spam", "Good", backend: redis_backend
